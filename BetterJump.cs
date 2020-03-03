d3bdf35b1add2641a8928adf793f98f1e8154b8d
Raghav Suriyashekar <42749141+tripwire1374@users.noreply.github.com>
2020-03-03 22:32:23 +0530

    Add files via upload

---
+++ BetterJump.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterJump : MonoBehaviour
+{
+    public Rigidbody rb;
+    [Range(1f, 15f)]
+    public float fallMultiplier = 2.5f;
+    [Range(1f, 15f)]
+    public float lowJumpMultiplier = 2f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * 1.2f * Time.deltaTime;
+        }
+        else if(rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
